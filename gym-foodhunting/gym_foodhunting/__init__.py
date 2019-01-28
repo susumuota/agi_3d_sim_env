20883b8383f716050e05ed712f381020929b7486
Susumu OTA <>
2019-01-29 03:14:12 +0900

    changed default to R2D2.

--- gym-foodhunting/gym_foodhunting/__init__.py
+++ gym-foodhunting/gym_foodhunting/__init__.py
@@ -1,42 +1,9 @@
 from gym.envs.registration import register
-from gym_foodhunting.foodhunting.gym_foodhunting import HSR, R2D2
-
-# FoodHunting HSR
-register(
-    id='FoodHunting-v0',
-    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
-    max_episode_steps=100,
-    reward_threshold=3.0,
-    kwargs={'render': False, 'discrete': False, 'robotModel': HSR}
-)
-
-register(
-    id='FoodHuntingGUI-v0',
-    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
-    max_episode_steps=100,
-    reward_threshold=3.0,
-    kwargs={'render': True, 'discrete': False, 'robotModel': HSR}
-)
-
-register(
-    id='FoodHuntingDiscrete-v0',
-    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
-    max_episode_steps=100,
-    reward_threshold=3.0,
-    kwargs={'render': False, 'discrete': True, 'robotModel': HSR}
-)
-
-register(
-    id='FoodHuntingDiscreteGUI-v0',
-    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
-    max_episode_steps=100,
-    reward_threshold=3.0,
-    kwargs={'render': True, 'discrete': True, 'robotModel': HSR}
-)
+from gym_foodhunting.foodhunting.gym_foodhunting import R2D2, HSR
 
 # FoodHunting R2D2
 register(
-    id='FoodHuntingR2D2-v0',
+    id='FoodHunting-v0',
     entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
     max_episode_steps=100,
     reward_threshold=3.0,
@@ -44,7 +11,7 @@ register(
 )
 
 register(
-    id='FoodHuntingR2D2GUI-v0',
+    id='FoodHuntingGUI-v0',
     entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
     max_episode_steps=100,
     reward_threshold=3.0,
@@ -52,7 +19,7 @@ register(
 )
 
 register(
-    id='FoodHuntingR2D2Discrete-v0',
+    id='FoodHuntingDiscrete-v0',
     entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
     max_episode_steps=100,
     reward_threshold=3.0,
@@ -60,9 +27,42 @@ register(
 )
 
 register(
-    id='FoodHuntingR2D2DiscreteGUI-v0',
+    id='FoodHuntingDiscreteGUI-v0',
     entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
     max_episode_steps=100,
     reward_threshold=3.0,
     kwargs={'render': True, 'discrete': True, 'robotModel': R2D2}
+)
+
+# FoodHunting HSR
+register(
+    id='FoodHuntingHSR-v0',
+    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
+    max_episode_steps=100,
+    reward_threshold=3.0,
+    kwargs={'render': False, 'discrete': False, 'robotModel': HSR}
+)
+
+register(
+    id='FoodHuntingHSRGUI-v0',
+    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
+    max_episode_steps=100,
+    reward_threshold=3.0,
+    kwargs={'render': True, 'discrete': False, 'robotModel': HSR}
+)
+
+register(
+    id='FoodHuntingHSRDiscrete-v0',
+    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
+    max_episode_steps=100,
+    reward_threshold=3.0,
+    kwargs={'render': False, 'discrete': True, 'robotModel': HSR}
+)
+
+register(
+    id='FoodHuntingHSRDiscreteGUI-v0',
+    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
+    max_episode_steps=100,
+    reward_threshold=3.0,
+    kwargs={'render': True, 'discrete': True, 'robotModel': HSR}
 )
