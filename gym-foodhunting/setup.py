e37853f794b4f43b3fc35d4603a9e16fc4cb7772
Susumu OTA <>
2019-01-29 00:57:06 +0900

    bumped version.

--- gym-foodhunting/setup.py
+++ gym-foodhunting/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_foodhunting',
-      version='0.0.1',
+      version='0.0.2',
       install_requires=['numpy', 'gym', 'pybullet'] # and stable-baselines for running RL
 )
