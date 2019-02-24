cefbd100e29f75196a64fb23f490e35fdce62f0c
Susumu OTA <>
2019-02-24 18:02:53 +0900

    added seed option.

--- gym-foodhunting/agents/ppo_agent.py
+++ gym-foodhunting/agents/ppo_agent.py
@@ -13,7 +13,7 @@ from stable_baselines import PPO2
 
 BEST_SAVE_FILE_SUFFIX = '_best'
 
-def make_env(env_name, rank, seed=0):
+def make_env(env_name, rank, seed):
     def _init():
         env = gym.make(env_name)
         env.seed(seed + rank)
@@ -21,19 +21,16 @@ def make_env(env_name, rank, seed=0):
     set_global_seeds(seed)
     return _init
 
-def learn(env_name, load_file, save_file, tensorboard_log, total_timesteps, n_cpu, reward_threshold):
+def learn(env_name, seed, load_file, save_file, tensorboard_log, total_timesteps, n_cpu, reward_threshold):
     best_mean_reward = -np.inf
     best_mean_step = np.inf
-    counter = 0
     save_file = env_name if save_file is None else save_file
     best_save_file = save_file + BEST_SAVE_FILE_SUFFIX
     start_time = time.time()
     def callback(_locals, _globals):
-        nonlocal best_mean_reward, best_mean_step, counter
-        counter += 1
-        if counter % 10 == 0:
-            t = int((time.time() - start_time) / 60.0)
-            print('minutes:', t)
+        nonlocal best_mean_reward, best_mean_step
+        t = int((time.time() - start_time) / 60.0)
+        print('minutes:', t)
         ep_info_buf = _locals['ep_info_buf']
         if len(ep_info_buf) < ep_info_buf.maxlen:
             return True
@@ -50,11 +47,12 @@ def learn(env_name, load_file, save_file, tensorboard_log, total_timesteps, n_cp
         return mean_reward < reward_threshold # False should finish learning
     # policy = CnnPolicy
     policy = CnnLstmPolicy
+    # policy = CnnLnLstmPolicy
     print(env_name, policy)
     # Run this to enable SubprocVecEnv on Mac OS X.
     # export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
     # see https://github.com/rtomayko/shotgun/issues/69#issuecomment-338401331
-    env = SubprocVecEnv([make_env(env_name, i) for i in range(n_cpu)])
+    env = SubprocVecEnv([make_env(env_name, i, seed) for i in range(n_cpu)])
     if load_file is not None:
         model = PPO2.load(load_file, env, verbose=1, tensorboard_log=tensorboard_log)
     else:
@@ -64,29 +62,27 @@ def learn(env_name, load_file, save_file, tensorboard_log, total_timesteps, n_cp
     model.save(save_file)
     env.close()
 
-def duplicate_obs(obss, n_cpu):
-    # before: (    1, 64, 64, 4)
-    # after:  (n_cpu, 64, 64, 4)
-    for i in range(n_cpu-1):
-        obss = np.insert(obss, [i+1], obss[0], axis=0) # padding obss[0]
-    return obss
 
-def play(env_name, load_file, total_timesteps, n_cpu):
+def play(env_name, seed, load_file, total_timesteps, n_cpu):
+    def padding_obss(obss, dummy_obss):
+        dummy_obss[ 0, :, :, : ] = obss
+        return dummy_obss
     # if it's GUI mode, number of env is changed to 1 to reduce GUI windows.
     # but trained LSTM model cannot change number of env.
     # so it needs to reshape observation by padding dummy data.
     isGUI = env_name.find('GUI') != -1
+    dummy_obss = np.zeros((n_cpu, 64, 64, 4)) if isGUI else None
+    env = SubprocVecEnv([make_env(env_name, i, seed) for i in range(1 if isGUI else n_cpu)])
     model = PPO2.load(load_file, verbose=1)
-    env = SubprocVecEnv([make_env(env_name, i) for i in range(1 if isGUI else n_cpu)])
     obss = env.reset()
-    obss = duplicate_obs(obss, n_cpu) if isGUI else obss
+    obss = padding_obss(obss, dummy_obss) if isGUI else obss
     rewards_buf = []
     steps_buf = []
     for i in range(total_timesteps):
         actions, _states = model.predict(obss)
         actions = actions[0:1] if isGUI else actions
         obss, rewards, dones, infos = env.step(actions)
-        obss = duplicate_obs(obss, n_cpu) if isGUI else obss
+        obss = padding_obss(obss, dummy_obss) if isGUI else obss
         # env.render() # dummy
         if dones.any():
             rewards_buf.extend([ info['episode']['r'] for info in infos if 'episode' in info ])
@@ -101,12 +97,13 @@ if __name__ == '__main__':
     parser.add_argument('--load_file', type=str, default=None, help='filename to load model.')
     parser.add_argument('--save_file', type=str, default=None, help='filename to save model.')
     parser.add_argument('--tensorboard_log', type=str, default=None, help='tensorboard log file.')
-    parser.add_argument('--total_timesteps', type=int, default=500000, help='total timesteps.')
+    parser.add_argument('--total_timesteps', type=int, default=10000000, help='total timesteps.')
     parser.add_argument('--n_cpu', type=int, default=8, help='number of CPU cores.')
+    parser.add_argument('--seed', type=int, default=0, help='seed for random number.')
     parser.add_argument('--reward_threshold', type=float, default=3.0, help='reward threshold to finish learning.')
     args = parser.parse_args()
 
     if args.play:
-        play(args.env_name, args.load_file, args.total_timesteps, args.n_cpu)
+        play(args.env_name, args.seed, args.load_file, args.total_timesteps, args.n_cpu)
     else:
-        learn(args.env_name, args.load_file, args.save_file, args.tensorboard_log, args.total_timesteps, args.n_cpu, args.reward_threshold)
+        learn(args.env_name, args.seed, args.load_file, args.save_file, args.tensorboard_log, args.total_timesteps, args.n_cpu, args.reward_threshold)
